75db5b07b099114e907894d951178b511a7f6b5c
Varshitha Uppula <varshithauppula@gmail.com>
2021-02-07 16:35:23 -0500

    update

--- src/main/webapp/UserAdmin/user-admin.controller.client.js
+++ src/main/webapp/UserAdmin/user-admin.controller.client.js
@@ -6,17 +6,47 @@ var $lastNameFld
 var $roleFld
 var $addUserBtn
 var tablebody
+var $updateUserBtn
 
 var userServices = new AdminUserServiceClient()
 
 
 
 function createUser(user) {
-    console.log(user)
-    usersArr.push(user)
-    renderUsers(usersArr)
+    userServices.createUser(user).then(function(actualCourse){
+
+        usersArr.push(actualCourse)
+        renderUsers(usersArr)
+    })
+
+}
+var selectedUser = null
+function selectUser(event) {
+
+    var selectBtn = jQuery(event.target)
+    console.log(selectBtn)
+    var theIndex1 = selectBtn.attr("id")
+    selectedUser = usersArr.find(user => user._id === theIndex1)
+    console.log(selectedUser)
+    $userNameFld.val(selectedUser.username)
+    $passwordFld.val(selectedUser.password)
+    $firstNameFld.val(selectedUser.firstname)
+    $lastNameFld.val(selectedUser.lastname)
+    $roleFld.val(selectedUser.role)
 }
 
+function deleteCourse (event) {
+    console.log(event.target)
+    var deleteBtn = jQuery(event.target)
+    var theIndex = deleteBtn.attr("id")
+
+    var theID = usersArr[theIndex]._id
+
+    userServices.deleteUser(theID).then(function (status) {
+        usersArr.splice(theIndex, 1)
+        renderUsers(usersArr)
+    })
+}
 
 function renderUsers(usersAr) {
     tablebody.empty()
@@ -30,28 +60,29 @@ function renderUsers(usersAr) {
             <td>${user.firstname}</td>
             <td>${user.lastname}</td>
             <td>${user.role}</td>
-            <td><span class="float-right" style="white-space: nowrap"><button class ="fa-2x fa fa-close wbdv-delete" id ="${i}"> </button>
-            <button class = "fa-2x fa fa-pencil"></button></span></td>
+            <td><span class="float-right" style="white-space: nowrap">
+            <button class ="fa-2x fa fa-close wbdv-delete" id ="${i}"> </button>
+            <button class = "fa-2x fa fa-pencil wbdv-edit" id ="${user._id}"></button></span></td>
           
         </tr>`)
     }
+jQuery(".wbdv-delete").click(deleteCourse)
 
 
-    jQuery(".wbdv-delete").click(function (event) {
-        console.log(event.target)
-        var deleteBtn = jQuery(event.target)
-        var theIndex = deleteBtn.attr("id")
 
-        var theID = usersArr[theIndex]._id
+    jQuery(".wbdv-edit").click(selectUser)
 
-        userServices.deleteUser(theID).then(function (status) {
-            usersArr.splice(theIndex, 1)
-            renderUsers(usersArr)
-        })
-    })
 
 }
 
+function updateUser(){
+    console.log(selectedUser)
+    selectedUser.username=$userNameFld.val()
+    selectedUser.password=$passwordFld.val()
+    selectedUser.firstname=$firstNameFld.val()
+    selectedUser.lastname=$lastNameFld.val()
+    selectedUser.role=$roleFld.val()
+}
 
 function main(){
      $userNameFld = $("#usernameFld")
@@ -60,8 +91,9 @@ function main(){
      $lastNameFld = $("#lastNameFld")
      $roleFld = $("#roleFld")
      $addUserBtn = jQuery(".wbdv-create")
+    $updateUserBtn =jQuery(".wbdv-update")
     tablebody = jQuery("tbody")
-
+    $updateUserBtn.click(updateUser)
     $addUserBtn.click(function () {
         var usersArr1 =
             {
