0bf97cb49634df6041f82399e659ed1ce531e3b8
Varshitha Uppula <varshithauppula@gmail.com>
2021-03-30 13:39:41 -0400

    modifications in pom file

--- src/main/java/com/example/myapp/models/Widget.java
+++ src/main/java/com/example/myapp/models/Widget.java
@@ -1,11 +1,45 @@
 package com.example.myapp.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="widgets")
 public class Widget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String type;
     private Integer size;
     private String text;
+    private Integer width;
+    private Integer height;
     private String topicId;
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
 
     public String getTopicId() {
         return topicId;
@@ -51,11 +85,39 @@ public class Widget {
     public Widget() {
     }
 
-    public Widget(Long id, String topicId, String type, Integer size, String text) {
+    public Widget(Long id, String type, Integer size, String text, Integer width, Integer height, String topicId) {
         this.id = id;
-        this.topicId = topicId;
         this.type = type;
         this.size = size;
         this.text = text;
+        this.width = width;
+        this.height = height;
+        this.topicId = topicId;
     }
 }
+
+/* create table widget (
+       id bigint not null,
+        height integer,
+        size integer,
+        text varchar(255),
+        topic_id varchar(255),
+        type varchar(255),
+        width integer,
+        primary key (id)
+    )*/
+
+/*
+ create table widgets (
+       id bigint not null auto_increment,
+        height integer,
+        size integer,
+        text varchar(255),
+        topic_id varchar(255),
+        type varchar(255),
+        width integer,
+        primary key (id)
+    ) */
+
+/* alter table widgets
+       add column name varchar(255)*/
--- src/main/java/com/example/myapp/services/WidgetService.java
+++ src/main/java/com/example/myapp/services/WidgetService.java
@@ -11,14 +11,7 @@ import java.util.List;
 public class WidgetService {
     private List<Widget> widgets = new ArrayList<Widget>();
     {
-        Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Welcome to Widgets");
-        Widget w2 = new Widget(234l, "ABC234", "PARAGRAPH", 1, "This is a paragraph");
-        Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Welcome to WebDev");
-        Widget w4 = new Widget(456l, "60566505bf36af0017e33f0b", "PARAGRAPH", 1, "Lorem ipsum");
-        widgets.add(w1);
-        widgets.add(w2);
-        widgets.add(w3);
-        widgets.add(w4);
+
     }
     // implement crud operations
     public Widget createWidgetForTopic(Widget widget) {
